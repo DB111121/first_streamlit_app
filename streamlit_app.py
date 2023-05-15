fdbddef04c56ed84d5e21b0898b93747f98950d1
DB111121 <133680653+DB111121@users.noreply.github.com>
2023-05-15 23:27:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.title('My parents new healthy diner')
+streamlit('Omega 3 and Blueberry oatmeal')
+streamlit('Hard boiled and free range eggs!')
