edb7d19fd43a372019c1db60e62b77a8bf54ba5a
DB111121 <133680653+DB111121@users.noreply.github.com>
2023-05-15 23:29:45 +0530

    Update streamlit_app.py
    added 3 text items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard boiled and free range eggs!')
